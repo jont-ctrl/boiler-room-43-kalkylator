77548834c643e377c9e4bb02c6da15827ba67c1d
gragas <180623g3h34htctrl@users.noreply.github.com>
2024-10-25 15:12:31 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+let calcActive = true;
+while (calcActive) {
+  let num1 = Number(prompt('Välj ett första tal'));
+  let chosenOperator = prompt('välj en operation + - % / * ^');
+  let num2 = Number(prompt('Vänligen ange ditt andra tal att räkna med'));
+  let result = 0;
+  if (isNaN(num1) && isNaN(num2)) {
+    console.log('Error, välj ett nummer');
+  } else {
+    switch (chosenOperator) {
+      case '+':
+        console.log(num1 + num2);
+        result = num1 + num2;
+        alert('Resultatet av din addition är ' + result);
+        break;
+      case '-':
+        console.log(num1 - num2);
+        result = num1 - num2;
+        alert('resultatet av din subrtakrion är ' + result);
+        break;
+      case '%':
+        console.log(num1 % num2);
+        result = num1 % num2;
+        alert('Resultatet av modulus är ' + result);
+        break;
+      case '/':
+        /*
+            num2 === 0 ? alert('Det går inte att dela med 0') : console.log(num1 / num2)
+            result = (num1 / num2)
+            alert('Resultatet av din division är '+result)
+            break; */
+        if (num2 === 0) {
+          alert('Det går inte att dela med 0');
+          break;
+        } else {
+          console.log(num1 / num2);
+          result = num1 / num2;
+          alert('Resultatet av din division är ' + result);
+          break;
+        }
+      case '*':
+        console.log(num1 * num2);
+        result = num1 * num2;
+        alert('resultatet av din multiplikation är ' + result);
+        break;
+      case '^':
+        console.log(num1 ** num2);
+        result = num1 ** num2;
+        alert('resultatet av din upphöjning är ' + result);
+        break;
+      default:
+        console.log('Error, fel operation');
+    }
+  }
+  calcActive = confirm('Vill du göra en ny beräkning?');
+}
